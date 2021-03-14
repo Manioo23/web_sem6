23bed3e45afb4008a80dd46763a1390150505244
Maciej Witkowski <maciekwitkowski322@gmail.com>
2021-03-14 15:28:18 +0100

    Basic project cleanup and structure

--- lab_2/src/App.js
+++ lab_2/src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<div>
+					Left side
+				</div>
+				<div>
+					<a>
+						Galeria
+					</a>
+					<a>
+						Artyści
+					</a>
+					<a>
+						Odnośniki
+					</a>
+				</div>
+			</header>
+			<main>
+				<section>
+					<h1>
+						Akwarele w sztuce
+					</h1>
+				</section>
+				<section>
+					<h2>
+						Galeria
+					</h2>
+					<div>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Fa%C5%82at_%C5%9Awite%C5%BA.jpg/440px-Fa%C5%82at_%C5%9Awite%C5%BA.jpg"
+							alt="Świteź"
+						/>
+					</div>
+					<div>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Fa%C5%82at_%C5%9Awite%C5%BA.jpg/440px-Fa%C5%82at_%C5%9Awite%C5%BA.jpg"
+							alt="Świteź"
+						/>
+					</div>
+					<div>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Fa%C5%82at_%C5%9Awite%C5%BA.jpg/440px-Fa%C5%82at_%C5%9Awite%C5%BA.jpg"
+							alt="Świteź"
+						/>
+					</div>
+				</section>
+				<div />
+				<section>
+					<h2>
+						Artyści
+					</h2>
+					<article>
+						<h3>
+							Juliusz Kossak
+						</h3>
+						<p>
+							Twórca wielkoformatowych akwarel o tematyce historycznej, przyrodniczej i folklorystycznej.
+							Uważany jest za mistrza techniki akwarelowej, pioniera polskiego malarstwa batalistycznego oraz klasyka malarstwa „literackiego".
+							Do ulubionych przez artystę tematów sztuki należały sceny z rycerskiej przeszłości Polaków, a także konie
+						</p>
+					</article>
+					<article>
+						<h3>
+							Juliusz Kossak
+						</h3>
+						<p>
+							Twórca wielkoformatowych akwarel o tematyce historycznej, przyrodniczej i folklorystycznej.
+							Uważany jest za mistrza techniki akwarelowej, pioniera polskiego malarstwa batalistycznego oraz klasyka malarstwa „literackiego".
+							Do ulubionych przez artystę tematów sztuki należały sceny z rycerskiej przeszłości Polaków, a także konie
+						</p>
+					</article>
+				</section>
+				<section>
+					<ul>
+						<li>
+							<a>
+								Link do muzeum sztuki w Warszawie
+							</a>
+						</li>
+						<li>
+							<a>
+								Link do muzeum sztuki w Warszawie
+							</a>
+						</li>
+						<li>
+							<a>
+								Link do muzeum sztuki w Warszawie
+							</a>
+						</li>
+						<li>
+							<a>
+								Link do muzeum sztuki w Warszawie
+							</a>
+						</li>
+						<li>
+							<a>
+								Link do muzeum sztuki w Warszawie
+							</a>
+						</li>
+					</ul>
+				</section>
+
+			</main>
+			<footer>
+				<p>
+					Strona stworzona przez
+					<span>
+						Szymon Panek
+					</span>
+					<span>
+						Maciej Witkowski
+					</span>
+				</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
