7864cf99d6f5aad47a6849a90e395838ee0429e9
Maciej Witkowski <maciekwitkowski322@gmail.com>
2021-03-28 21:57:12 +0200

    Quick Fix Checkout service

--- lab_4/books/src/main/java/pl/edu/pwr/ztw/books/services/CheckoutService.java
+++ lab_4/books/src/main/java/pl/edu/pwr/ztw/books/services/CheckoutService.java
@@ -8,6 +8,6 @@ import pl.edu.pwr.ztw.books.models.Checkout;
 import java.util.List;
 
 public interface CheckoutService extends JpaRepository<Checkout, Long> {
-    @Query("SELECT c from Checkout c where c.book_id =:book_id ")       // using @query
+    @Query("SELECT c from Checkout c where c.book =:book_id ")       // using @query
     List<Checkout> findByBook(@Param("book_id") Long book_id);
 }
