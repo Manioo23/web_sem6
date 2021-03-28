b17f0b11012cb9bf738a20561551882089435d25
HoSzyk <57402320+HoSzyk@users.noreply.github.com>
2021-03-28 17:17:28 +0200

    Post

--- lab_4/books/src/main/java/pl/edu/pwr/ztw/books/controllers/BookController.java
+++ lab_4/books/src/main/java/pl/edu/pwr/ztw/books/controllers/BookController.java
@@ -3,9 +3,8 @@ package pl.edu.pwr.ztw.books.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.pwr.ztw.books.models.Book;
 import pl.edu.pwr.ztw.books.services.BookService;
 
 @RestController
@@ -18,5 +17,17 @@ public class BookController {
     public ResponseEntity<Object> getBooks()    {
         return new ResponseEntity<>(bookService.findAll(), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/get/books/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getBook(@PathVariable("id") Long id)    {
+        return new ResponseEntity<>(bookService.getOne(id), HttpStatus.OK);
+    }
+
+    @PostMapping("books")
+    public ResponseEntity<Object> addBook(@RequestBody Book newBook)    {
+        return new ResponseEntity<>(bookService.save(newBook), HttpStatus.CREATED);
+    }
+
+
 }
 
