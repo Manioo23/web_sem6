ee29e71c8d6d4340f34f3ba827faf2378ba5b7cc
HoSzyk <57402320+HoSzyk@users.noreply.github.com>
2021-03-28 17:58:51 +0200

    book crud

--- lab_4/books/src/main/java/pl/edu/pwr/ztw/books/controllers/BookController.java
+++ lab_4/books/src/main/java/pl/edu/pwr/ztw/books/controllers/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import pl.edu.pwr.ztw.books.models.Book;
 import pl.edu.pwr.ztw.books.services.BookService;
 
+import java.util.Optional;
+
 @RestController
 public class BookController {
 
@@ -14,18 +16,30 @@ public class BookController {
     BookService bookService;
 
     @RequestMapping(value = "/get/books", method = RequestMethod.GET)
-    public ResponseEntity<Object> getBooks()    {
+    public ResponseEntity<Object> getBooks() {
         return new ResponseEntity<>(bookService.findAll(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/get/books/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Object> getBook(@PathVariable("id") Long id)    {
+    public ResponseEntity<Object> getBook(@PathVariable("id") Long id) {
         return new ResponseEntity<>(bookService.getOne(id), HttpStatus.OK);
     }
 
-    @PostMapping("books")
-    public ResponseEntity<Object> addBook(@RequestBody Book newBook)    {
-        return new ResponseEntity<>(bookService.save(newBook), HttpStatus.CREATED);
+    @RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> replaceBook(@RequestBody Book newBook) {
+        return new ResponseEntity<>(bookService.save(newBook), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteBook(@PathVariable("id") Long id) {
+        Optional<Book> item = bookService.findById(id);
+        if (item.isPresent()){
+            bookService.delete(item.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 
 
