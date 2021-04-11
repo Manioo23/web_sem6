f2b0f298aeb3c1bebdc11b73d40f310615b551ec
Maciej Witkowski <maciekwitkowski322@gmail.com>
2021-04-11 23:48:12 +0200

    Init lab_5 project

---
+++ lab_5/src/components/PersonsForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="person-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Imię i nazwisko</label>
+      <input type="text" />
+      <label>Email</label>
+      <input type="text" />
+      <label>Telefon</label>
+      <input type="text" />
+      <button>Dodaj kontakt</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "person-form",
+  data() {
+    return {
+      person: {
+        name: "",
+        email: "",
+        phone: "",
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log("uruchomiono handleSubmit");
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 2rem;
+}
+</style>
