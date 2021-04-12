dfe4b74571d274f71e00a44aa6377bea0a0cf70d
HoSzyk <57402320+HoSzyk@users.noreply.github.com>
2021-04-12 15:02:44 +0200

    commit

---
+++ lab_5/src/components/BookForm.vue
@@ -0,0 +1,42 @@
+<template>
+     <div id="book-form">
+        <form @submit.prevent="handleSubmit">
+            <label>Tytuł</label>
+            <input type="text" v-model="book.title"/>
+            <label>Liczba stron</label>
+            <input type="number" v-model="book.pages"/>
+            <label>Autor</label>
+            <select v-model="book.author.id">
+                <option v-for="author in authorSource" :key="author.id" :value="author.id">{{author.firstName + ' ' + author.lastName}}</option>
+            </select>
+            <button>Zapisz książkę</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'book-form',
+        props: {
+            authorSource: Array
+        },
+        data() {
+            return {
+                book: {
+                    title: '',
+                    pages: 0,
+                    author: {
+                        id: ''
+                    }
+                }
+            }
+        },
+        methods: {
+            handleSubmit(){
+                this.$emit('add:book', this.book)
+            },
+        }
+    }
+
+</script>
---
+++ lab_5/src/layouts/AddBook.vue
@@ -0,0 +1,61 @@
+<template>
+     <div class="small-container">
+        <h1>Dodaj Książkę</h1>
+        <book-form @add:book="addBook" :authorSource="authors"/>
+    </div>
+</template>
+
+<script>
+    import BookForm from '@/components/BookForm.vue'
+
+    export default {
+        name: 'add-book',
+        components: {
+            BookForm,
+        },
+        data() {
+            return {
+                authors: [{
+                    id:1,
+                    firstName: "Adam",
+                    lastName: "Mickiewicz"
+                }]
+            }
+        },
+
+        methods: {
+            async getAuthors() {
+                try {
+                    const response = await fetch('http://localhost:8080/get/authors')
+                    const data = await response.json()
+                    this.authors = data
+                } catch (error) {
+                    console.error(error)
+                }
+            },
+
+            async addBook(book) {
+                const request = new Request('http://localhost:8080/create/book',{
+                    method: "PUT",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(book)
+                });
+                try {
+                    await fetch(request)
+                } catch (error) {
+                    console.error(error)
+                }
+            },
+
+        },
+
+        mounted() {
+                this.getAuthors()
+            },
+
+    }
+
+</script>
