3e940247f0ceb7bae60c28ac3d0c0d10d3862a4a
HoSzyk <57402320+HoSzyk@users.noreply.github.com>
2021-04-12 18:18:01 +0200

    Update Books.vue

--- lab_5/src/pages/Books.vue
+++ lab_5/src/pages/Books.vue
@@ -1,59 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <main-layout>
-    <div class="small-container">
-      <h1>Książki</h1>
-      <book-table :bookSource="books" />
-    </div>
-  </main-layout>
-</template>
-
-<script>
-import BookTable from "@/components/BookTable.vue";
-import MainLayout from "@/layouts/MainLayout.vue";
-
-export default {
-  name: "books",
-  components: {
-    BookTable,
-    MainLayout,
-  },
-  data() {
-    return {
-      books: [
-        {
-          id: 1,
-          title: "Kot w butach",
-          author: {
-            id: 1,
-            firstName: "Jan",
-            lastName: "Brzechwa",
-          },
-          pages: 123,
-        },
-      ],
-    };
-  },
-  methods: {
-    async getBooks() {
-      const request = new Request("http://localhost:8081/get/books", {
-        method: "GET",
-      });
-      try {
-        const response = await fetch(request);
-        const data = await response.json();
-        this.books = data;
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
-
-  mounted() {
-    this.getBooks();
-  },
-};
-=======
     <main-layout>
         <div class="small-container">
             <h1>Książki</h1>
@@ -113,5 +58,4 @@ export default {
         }
     }
 
->>>>>>> 92d9846652def0c7015b298496fcbe217760b7ca
 </script>
